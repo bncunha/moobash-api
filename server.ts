40ea35719e366bbe6fcd14062c09367bdb9f2ff4
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-05-17 18:04:10 -0300

    fx build

--- server.ts
+++ server.ts
@@ -16,6 +16,6 @@ app.get('/teste', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-    console.log('App is listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App is listening on port: ' + process.env.PORT || 3000);
 });
