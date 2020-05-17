65815ecd9c54e8d23dde8c0b3d8373eeab822483
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-05-17 17:42:01 -0300

    config database

--- src/dao/DatabaseConnection.ts
+++ src/dao/DatabaseConnection.ts
@@ -5,12 +5,13 @@ export class DataBaseConnection {
     async initConnection() {
         try {
             const res = await createConnection({
+                url: process.env.BD_CON,
                 type: "mysql",
-                host: "localhost",
-                port: 3306,
-                username: "root",
-                password: "123456",
-                database: "bek_system_dev",
+                // host: "localhost",
+                // port: 3306,
+                // username: "root",
+                // password: "123456",
+                // database: "bek_system_dev",
                 entities: [
                     __dirname + "./../models/*.js"
                 ],
